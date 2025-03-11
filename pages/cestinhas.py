dd312e6ca5abf265be4ad78f1097412a8d94b3bf
diego gomes de santana <diego.bakugan@hotmail.com>
2025-03-11 14:39:50 -0300

    feat: lideres de assistencias por time

--- pages/cestinhas.py
+++ pages/cestinhas.py
@@ -9,6 +9,17 @@ st.title("Cestinhas")
 st.divider()
 
 
+st.title("Líderes de Assistências por Time")
+st.divider()
+@st.cache_data  
+def load_data():
+    return pd.read_csv("dados/2023-2024_NBA_Player_Stats_Regular.csv", sep=";",encoding='iso-8859-1')
+data = load_data()
+data = data[['Player', 'Tm', 'AST']]
+leaders = data.loc[data.groupby('Tm')['AST'].idxmax()]
+leaders = leaders.sort_values(by='AST', ascending=False)
+st.dataframe(leaders[['Player', 'Tm', 'AST']].reset_index(drop=True))
+
 
 
 
