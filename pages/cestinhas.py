e10e7b963c2e0b9e10a466eaf28a1eb748fb332c
Paulina Santos <kaspaulina@hotmail.com>
2025-03-13 21:08:56 -0300

    feat: lideres de pontos por time

--- pages/cestinhas.py
+++ pages/cestinhas.py
@@ -13,12 +13,27 @@ def load_data():
     return pd.read_csv("dados/2023-2024_NBA_Player_Stats_Regular.csv", sep=";",encoding='iso-8859-1')
 data = load_data()
 
-st.title("Líderes de Assistências por Time")
+st.subheader("Líderes de Pontos por Time")
+data_leaders_pts = data[['Player', 'Tm', 'PTS']]
+data_leaders_pts = data_leaders_pts[data_leaders_pts.Tm!="TOT"]
+leaders_pts = data_leaders_pts.loc[data_leaders_pts.groupby('Tm')['PTS'].idxmax()]
+leaders_pts = leaders_pts.sort_values(by='PTS', ascending=False)
+st.dataframe(leaders_pts[['Player', 'Tm', 'PTS']].reset_index(drop=True))
+
+st.metric("Média de Pontos considerando os maiores pontuadores", round(leaders_pts.PTS.mean(), 1))
+
 st.divider()
+
+st.subheader("Líderes de Assistências por Time")
 data_leaders = data[['Player', 'Tm', 'AST']]
+data_leaders = data_leaders[data_leaders.Tm!="TOT"]
 leaders = data_leaders.loc[data_leaders.groupby('Tm')['AST'].idxmax()]
 leaders = leaders.sort_values(by='AST', ascending=False)
 st.dataframe(leaders[['Player', 'Tm', 'AST']].reset_index(drop=True))
+
+
+
+
 
 
 
